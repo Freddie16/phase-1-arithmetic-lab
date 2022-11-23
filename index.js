18647613acaddcfb44e947465583a28373d7a3af
Freddie16 <freddiemurigi@gmail.com>
2022-11-23 14:46:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+let multiply = (num1 * num2 )
+
+let random = Math.ceil(Math.random())
+
+
+const num3 = 65
+const num4 = 14
+let mod=Math.floor(num3/num4)
+
+
+let max = Math.max(5,9,14,20)
